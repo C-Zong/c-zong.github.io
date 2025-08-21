770d427378d3ceee4f484df0437638e807615fd2
C. Zong <chenyangzong621@gmail.com>
2025-08-21 19:35:43 -0400

    feat: finalize personal website layout and content

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+// Theme toggle
+document.getElementById("config").theme.addEventListener("click", () => {
+  let theme = localStorage.getItem("theme");
+  theme = theme === "light" ? "dark" : "light";
+  document.body.setAttribute("data-bs-theme", theme);
+  localStorage.setItem("theme", theme);
+});
